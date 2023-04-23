5c8696a79e3f43b31e0cc2eb76d3c86f10140fce
austinj1522 <austin.logan.john@gmail.com>
2023-04-23 13:14:40 -0500

    init commit

---
+++ main.py
@@ -0,0 +1,256 @@
+from flask import Flask, jsonify, request, abort
+from redis import Redis, RedisError
+import redis
+import hashlib                                              # used for MD5 Hash
+import requests                                             # used for slack alert
+import os
+import socket
+import json 
+
+
+r = redis.Redis(host="myredis", port=6379, decode_responses=True)
+
+
+app = Flask(__name__)
+
+slackURL = "" 
+
+#default local host page
+@app.route("/")
+
+def hello_world():
+    return "<p>Howdy! We are Group 4. This is our API.</p>"
+
+
+#MD5
+@app.route("/md5/<string:strvalue>")
+
+def md5(strvalue):
+    md5val = hashlib.md5(strvalue.encode())
+    hexval = md5val.hexdigest()
+    return jsonify(
+        input=strvalue,
+        output=hexval
+    )
+
+
+#factorial
+@app.route("/factorial/<int:x>")
+
+def factorial(x):
+
+    fact = 1
+    if x < 0:
+        return jsonify(
+            input=x,
+            output="Error, the input must be a positive integer"
+        )       
+    elif x == 0:
+        return jsonify(
+            input=x,
+            output=1
+        )    
+    else:
+        for i in range(1,x+1):
+            fact = fact * i
+        return jsonify(
+            input=x,
+            output=fact
+        )
+     
+        
+# fibonacci
+@app.route("/fibonacci/<int:x>")
+
+def fibonacci_num(x):
+    num1= 0
+    num2 = 1
+    seq=[0]
+
+    if x < 0:
+        return jsonify(
+            input=x,
+            output="Please enter a positive integer"
+        )
+       
+    elif x == 0:
+        return jsonify(
+            input=x,
+            output=seq
+            )
+
+    elif x == 1:
+        while num2 < 2:
+            seq.append(num2)
+            num1, num2 = num2, num1+num2
+        return jsonify(
+            input=x,
+            output=seq
+        )
+
+    else:
+        while num2 <= x:
+            seq.append(num2)
+            num1, num2 = num2, num1+num2
+            
+        return jsonify(
+            input=x, 
+            output=seq
+        )              
+
+
+# is-prime
+@app.route("/is-prime/<int:n>")
+
+def prime(n):
+    flag = True
+    
+    if n == 1 or n == 0:
+        flag = False
+        return jsonify(
+            input=n,
+            output=flag
+        )
+    elif n > 1:
+        for i in range(2, n):
+            if n % i == 0:
+                flag = False
+                return jsonify(
+                    input=n,
+                    output=flag
+                )
+    else:
+        return jsonify(
+            input=n,
+            output="Error, Input is Invalid"
+        )
+    return jsonify(
+        input=n,
+        output=flag
+    )    
+
+
+#slack-alert
+@app.route("/slack-alert/<string:post>")
+
+def slack_alert(post):
+    flag = True
+    response = requests.post(slackURL, json={'text': post, 'username':"Group4restAPI_Bot"})
+    if response.status_code == 200:
+        return jsonify(
+            input=post,
+            output=flag
+        )
+    else:
+        flag = False
+        return jsonify(
+            input=post,
+            output=flag
+        )
+
+
+@app.route("/keyval", methods=[ "POST", "PUT"])
+def set():
+    data = request.get_json()
+    
+    if request.method == 'POST':
+       command = "Create " + data["key"] + "/" + data["value"]
+       if r.exists(data["key"]):
+           #create object to return if it exists
+           keypair_found = {
+               "storage-key": data["key"],
+               "storage-val": data["value"],
+               "command": command,
+               "result": False,
+               "error": "Unable to add key pair: key already exists"
+           } 
+           return jsonify(keypair_found), abort(409)
+       else:
+           key = data["key"]
+           value = data[value]
+           r.set(key, value)
+
+           keypair = {
+               "storage-key": data["key"],
+               "storage-val": data["value"],
+               "command": command,
+               "result": True,
+               "error": ""
+           }
+           return jsonify(keypair)
+    elif request.method == "PUT":
+        command = "Update " + data["key"] + "/" + data["value"]
+        key = data["key"]
+        value = data["value"]
+        if r.exists(data["key"]):
+            r.set(key,value)
+            keypair = {
+               "storage-key": data["key"],
+               "storage-val": data["value"],
+               "command": command,
+               "result": True,
+               "error": ""
+           }
+            return jsonify(keypair)
+        else:
+            keypair_notfound = {
+               "storage-key": data["key"],
+               "storage-val": data["value"],
+               "command": command,
+               "result": False,
+               "error": "Key does not exist"
+           } 
+            return jsonify(keypair_notfound), abort(404)
+
+
+@app.route("/keyval/<string:inputval>", methods=["GET", "DELETE"])
+def get(inputval):
+    
+    if request.method =="GET":
+        command = "READ value for the following key: " + inputval
+        if r.exists(inputval):
+            value = r.get(inputval)
+            keypair = {
+               "storage-key": inputval,
+               "storage-val": value,
+               "command": command,
+               "result": True,
+               "error": ""
+           }
+            return jsonify(keypair)
+        else:
+             keypair_notfound = {
+               "storage-key": inputval,
+               "storage-val": "Not found",
+               "command": command,
+               "result": False,
+               "error": "Key does not exist"
+           }
+             return jsonify(keypair_notfound), abort(404)
+
+    elif request.method == "DELETE":
+        command = "Delete the stored value for key: " + inputval
+        if r.exists(inputval) == 1: # 1 is True
+            value = r.get(inputval)
+            r.delete(inputval)
+            keypair_deleted = {
+               "storage-key": inputval,
+               "storage-val": value,
+               "command": command,
+               "result": True,
+               "error": "Key pair was found and deleted from database"
+           }
+            return jsonify(keypair_deleted)
+        else:
+            keypair_notfound = {
+               "storage-key": inputval,
+               "storage-val": "Not found",
+               "command": command,
+               "result": False,
+               "error": "Unable to delete key: Key does not exist"
+           }
+            return jsonify(keypair_notfound), abort(404)
+
+   
+if __name__ == "__main__":                                  # debug mode for testing, port 4000 as per assignment instructions
+    app.run(host='0.0.0.0',port=4000, debug=True)
---
+++ test.py
@@ -0,0 +1,192 @@
+import requests
+
+
+errors = 0
+
+#############################
+# md5 test 
+
+response = requests.get("http://localhost:4000/md5/Hello World")
+data = response.json()
+if response.status_code == 200 or response.status_code == 404:
+    print("Test Passed")
+else:
+    print("Test Failed")
+    errors += 1
+
+response = requests.get("http://localhost:4000/md5/    white   space")
+data = response.json()
+if response.status_code == 200 or response.status_code == 404:
+    print("Test Passed")
+else:
+    print("Test Failed")
+    errors += 1
+
+response = requests.get("http://localhost:4000/md5/37258943570265410")
+data = response.json()
+if response.status_code == 200 or response.status_code == 404:
+    print("Test Passed")
+else:
+    print("Test Failed")
+    errors += 1
+
+response = requests.get("http://localhost:4000/md5/String_Value")
+data = response.json()
+if response.status_code == 200 or response.status_code == 404:
+    print("Test Passed")
+else:
+    print("Test Failed")
+    errors += 1
+
+response = requests.get("http://localhost:4000/md5/GROUP4 TEST CASES")
+data = response.json()
+if response.status_code == 200 or response.status_code == 404:
+    print("Test Passed")
+else:
+    print("Test Failed")
+    errors += 1
+
+
+#############################
+# factorial test 
+
+response = requests.get("http://localhost:4000/factorial/20")
+data = response.json()
+if ((response.status_code == 200) and data['output'] == 2432902008176640000):
+    print("Test Passed")
+else:
+    print("Test Failed")
+    errors += 1
+
+
+response = requests.get("http://localhost:4000/factorial/100")
+data = response.json()
+if ((response.status_code == 200) and data['output'] == 93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000):
+    print("Test Passed")
+else:
+    print("Test Failed")
+    errors += 1
+    
+
+
+response = requests.get("http://localhost:4000/factorial/0")
+data = response.json()
+if ((response.status_code == 200) and data['output'] == 1):
+    print("Test Passed")
+else:
+    print("Test Failed")
+    errors += 1
+
+response = requests.get("http://localhost:4000/factorial/1")
+data = response.json()
+if ((response.status_code == 200) and data['output'] == 1):
+    print("Test Passed")
+else:
+    print("Test Failed")
+    errors += 1
+
+#############################
+# fibonacci test 
+
+response = requests.get("http://localhost:4000/fibonacci/2355463")
+data = response.json()
+if response.status_code == 200:
+    print("Test Passed")
+else:
+    print("Test Failed")
+    errors += 1
+
+response = requests.get("http://localhost:4000/fibonacci/31")
+data = response.json()
+if response.status_code == 200:
+    print("Test Passed")
+else:
+    print("Test Failed")
+    errors += 1
+
+response = requests.get("http://localhost:4000/fibonacci/10")
+data = response.json()
+if response.status_code == 200:
+    print("Test Passed")
+else:
+    print("Test Failed")
+    errors += 1
+
+response = requests.get("http://localhost:4000/fibonacci/239402548032")
+data = response.json()
+if response.status_code == 200:
+    print("Test Passed")
+else:
+    print("Test Failed")
+    errors += 1
+
+response = requests.get("http://localhost:4000/fibonacci/0")
+data = response.json()
+if response.status_code == 200:
+    print("Test Passed")
+else:
+    print("Test Failed")
+    errors += 1
+
+#############################
+# is prime test 
+
+response = requests.get("http://localhost:4000/is-prime/4")
+data = response.json()
+if response.status_code == 200:
+    print("Test Passed")
+else:
+    print("Test Failed")
+    errors += 1
+
+response = requests.get("http://localhost:4000/is-prime/32478329481")
+data = response.json()
+if response.status_code == 200:
+    print("Test Passed")
+else:
+    print("Test Failed")
+    errors += 1
+
+response = requests.get("http://localhost:4000/is-prime/0")
+data = response.json()
+if response.status_code == 200:
+    print("Test Passed")
+else:
+    print("Test Failed")
+    errors += 1
+
+response = requests.get("http://localhost:4000/is-prime/1")
+data = response.json()
+if response.status_code == 200:
+    print("Test Passed")
+else:
+    print("Test Failed")
+    errors += 1
+
+response = requests.get("http://localhost:4000/is-prime/1000000000")
+data = response.json()
+if response.status_code == 200:
+    print("Test Passed")
+else:
+    print("Test Failed")
+    errors += 1
+
+#############################
+# slack alert - only one test as this test is primarily for functionality, input would not matter as the function is built to take in anything as input and post it directly to our Slack channel.
+
+#response = requests.get("http://localhost:4000/slack-alert/Post this string into our Slack Channel to test API functionality")
+#data = response.json()
+#if response.status_code == 200:
+#    print("Test Passed")
+#else:
+#    print("Test Failed")
+#    errors += 1
+
+
+def noErrors():
+    return 0
+
+if errors == 0:
+    noErrors()
+else:
+    print("You have errors within your API. Edit accordingly and rerun test file!")
